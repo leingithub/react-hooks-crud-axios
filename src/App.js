dd29dd66faa870315051bc964adc5b68d6142f9d
leingithub <leingithub@gmail.com>
2023-04-14 19:43:29 -0400

    primer commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import AddTutorial from "./components/AddTutorial";
+import Tutorial from "./components/Tutorial";
+import TutorialsList from "./components/TutorialsList";
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          Lein
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+              Tutoriales
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Grabar
+            </Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/" element={<TutorialsList/>} />
+          <Route path="/tutorials" element={<TutorialsList/>} />
+          <Route path="/add" element={<AddTutorial/>} />
+          <Route path="/tutorials/:id" element={<Tutorial/>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
